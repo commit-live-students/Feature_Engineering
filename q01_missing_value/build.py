e32018cc6540a3720832d44d0d0580bae7295665
mafia0723 <32430696+mafia0723@users.noreply.github.com>
2017-10-16 15:35:25 +0530

    Update build.py

--- q01_missing_value/build.py
+++ q01_missing_value/build.py
@@ -5,7 +5,7 @@ import pandas as pd
 ny_housing = pd.read_csv('data/train.csv')
 # Selecting 4 most relevant variables along with target variable from the dataset fot the Cleaning and Preprocessing.
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
-
+test
 
 # Write your code here:
 
