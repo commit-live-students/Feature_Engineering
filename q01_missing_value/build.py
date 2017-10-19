5f287ad87cb60f2783ff97e3029d1eb12b35519f
rohanhg91 <rohanhgupta91@gmail.com>
2017-10-19 09:59:14 +0000

    Done

--- q01_missing_value/build.py
+++ q01_missing_value/build.py
@@ -1,5 +1,6 @@
 # Default imports
 import pandas as pd
+import numpy as np
 
 # Data loading
 ny_housing = pd.read_csv('data/train.csv')
@@ -8,4 +9,14 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 
 # Write your code here:
+def imputation(dataset):
+    for i in dataset.columns:
+        if dataset[i].dtypes == 'int64' or dataset[i].dtypes=='float64':
+            dataset[i] = dataset[i].fillna(dataset[i].mean(),axis=0)
+        elif dataset[i].dtypes == 'object':
+            dataset[i] = dataset[i].fillna(dataset[i].mode()[0],axis=0)
 
+    data1 = dataset.select_dtypes(include=[np.float64,np.int64])
+    data2 = dataset.select_dtypes(include=[np.object])
+
+    return data1,data2
