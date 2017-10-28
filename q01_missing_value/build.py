9c485e2f008af19c780e128c624988121fc6b4db
apoorvabanubakode <apoorva.banubakode@gmail.com>
2017-10-28 10:39:30 +0000

    Done

--- q01_missing_value/build.py
+++ q01_missing_value/build.py
@@ -1,6 +1,6 @@
 # Default imports
 import pandas as pd
-
+from sklearn.preprocessing import Imputer
 # Data loading
 ny_housing = pd.read_csv('data/train.csv')
 # Selecting 4 most relevant variables along with target variable from the dataset fot the Cleaning and Preprocessing.
@@ -8,4 +8,30 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 
 # Write your code here:
+# Default imports
+#import pandas as pd
 
+
+# Data loading
+#ny_housing = pd.read_csv('data/train.csv')
+# Selecting 4 most relevant variables along with target variable from the dataset fot the Cleaning and Preprocessing.
+#housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
+
+def imputation(df):
+    categorical=df.loc[:,['LotShape','GarageType']]
+    numeric=df.loc[:,['MasVnrArea','GrLivArea','SalePrice']]
+    numericheader=list(numeric)
+
+    imp_mean = Imputer(missing_values = 'NaN', strategy='mean')
+
+    for i in numericheader:
+        imp_mean.fit(numeric[[i]])
+        numeric[i] = imp_mean.transform(numeric[[i]])
+    #print(numeric)
+
+    categorical['LotShape']=categorical.loc[:,'LotShape'].fillna(categorical.loc[:,'LotShape'].mode()[0])
+    #print categorical
+
+    categorical['GarageType']=categorical.loc[:,'GarageType'].fillna(categorical.loc[:,'GarageType'].mode()[0])
+    #print categorical
+    return numeric,categorical
