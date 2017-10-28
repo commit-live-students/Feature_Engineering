f92fe26901adce7bb500eac21cc7e82a6ff0f11b
mritunjaysingh07 <mj.singh07@gmail.com>
2017-10-28 21:12:20 +0000

    Done

--- q02_outlier_removal/build.py
+++ q02_outlier_removal/build.py
@@ -6,6 +6,14 @@ ny_housing = pd.read_csv('data/train.csv')
 # Selecting 4 most relevant variables from the dataset fot the Cleaning and Preprocessing.
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
 
+def outlier_removal(housing_data):
+    df = housing_data
 
+    q = df["MasVnrArea"].quantile(0.95)
+    df1 = df[df["MasVnrArea"] < q]
+    q1 = df["GrLivArea"].quantile(0.95)
+    df2 = df[df["GrLivArea"] < q1]
+    q2 = df["SalePrice"].quantile(0.95)
+    df3 = df[df["SalePrice"] < q2]
+    return df3
 # Write your code here:
-
--- q03_skewness_log/build.py
+++ q03_skewness_log/build.py
@@ -4,6 +4,19 @@ import numpy as np
 
 data = pd.read_csv('data/train.csv')
 
+def skewness_log(data):
+    df_trans=data.copy()
+    df_trans['SalePrice'] = np.log(df_trans['SalePrice'])
+    skewness_SalePrice = skew(df_trans['SalePrice'])
+    #print(skewness_SalePrice)
+
+    df_trans['GrLivArea'] = np.log(df_trans['GrLivArea'])
+    skewness_GrLivArea = skew(df_trans['GrLivArea'])
+   #print(skewness_GrLivArea)
+    #skewed_grLiv = skew(df['SalePrice'])
+   # print(skewed_grLiv)
+
+    return skewness_GrLivArea,skewness_SalePrice
+
 
 # Write code here:
-
