58bc783f0a7f4e6e9822a52d0e063e6d69044010
samir0719 <sameer_0719@yahoo.com>
2017-10-29 17:22:13 +0000

    Done

--- q02_outlier_removal/build.py
+++ q02_outlier_removal/build.py
@@ -1,3 +1,4 @@
+# %load q02_outlier_removal/build.py
 # Default imports
 import pandas as pd
 
@@ -5,7 +6,11 @@ import pandas as pd
 ny_housing = pd.read_csv('data/train.csv')
 # Selecting 4 most relevant variables from the dataset fot the Cleaning and Preprocessing.
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
-
-
-# Write your code here:
-
+def outlier_removal(housing_data):
+    high = .95
+    quant_df = housing_data.quantile(high)
+    df = housing_data.drop(housing_data[(housing_data['GrLivArea']>quant_df['GrLivArea']) |
+                                    (housing_data['SalePrice']>quant_df['SalePrice'])|
+                                    (housing_data['MasVnrArea']>quant_df['MasVnrArea'])].index)
+#housing_data[(housing_data['GrLivArea']>quant_df['GrLivArea']) | (housing_data['GrLivArea']<quant1_df['GrLivArea'])]
+    return df
--- q04_encoding/build.py
+++ q04_encoding/build.py
@@ -1,4 +1,3 @@
-# Default imports
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
@@ -6,5 +5,10 @@ ny_housing = pd.read_csv('data/train.csv')
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
 
 
-# Write your code here:
-
+def encoding (dataset):
+    label_enc= LabelEncoder()
+    a = pd.get_dummies(housing_data['GarageType'], drop_first=True)
+    housing_data['LotShape'] = label_enc.fit_transform(housing_data['LotShape'])
+    b = housing_data['LotShape']
+    df_new = pd.concat([housing_data,a,b], axis = 1)
+    return df_new
