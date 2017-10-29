d768138b77a35bf59db200fe61c66fd7cfef4efe
jyotika03 <jyotika.tewari1992@yahoo.com>
2017-10-29 19:14:11 +0000

    Done

--- q03_skewness_log/build.py
+++ q03_skewness_log/build.py
@@ -1,9 +1,15 @@
+# Default imports
 from scipy.stats import skew
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv('data/train.csv')
+ny_housing = pd.read_csv('data/train.csv')
 
+def skewness_sqrt(data):
+    data['SalePrice'] = np.sqrt(data['SalePrice'])
+    data['GrLivArea'] = np.sqrt(data['GrLivArea'])
 
-# Write code here:
+    skew_1 = skew(data['GrLivArea'])
+    skew_2 = skew(data['SalePrice'])
 
+    return skew_1, skew_2
--- q04_encoding/build.py
+++ q04_encoding/build.py
@@ -1,10 +1,16 @@
+# %load q04_encoding/build.py
 # Default imports
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-
 ny_housing = pd.read_csv('data/train.csv')
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
-
-
 # Write your code here:
+def encoding(df):
+    labelencoder = LabelEncoder()
+    df['LotShape'] = labelencoder.fit_transform(df['LotShape']) #     encode with label encoder
 
+    # encode with one_hot_encoding
+    new_df = pd.get_dummies(df['GarageType'], drop_first=False)
+    df = pd.concat([df, new_df], axis=1)
+
+    return df
