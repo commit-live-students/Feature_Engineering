dbf6523b6565526e3bb3366ed2a79ca298a0ef44
shashi07 <shashi.mishra43@gmail.com>
2017-10-31 05:17:39 +0000

    Done

--- q04_encoding/build.py
+++ q04_encoding/build.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import LabelEncoder
 ny_housing = pd.read_csv('data/train.csv')
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
 
-
+def encoding(dataset):
+    le = LabelEncoder()
+    return pd.get_dummies(dataset, drop_first=True)
 # Write your code here:
-
