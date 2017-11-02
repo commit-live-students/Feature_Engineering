d88cbe7024d7ef6da9ec9a675e4c9fbfb1c0c8d6
msonumanish <msonu.manish@gmail.com>
2017-11-02 06:28:03 +0000

    Done

--- q02_outlier_removal/build.py
+++ q02_outlier_removal/build.py
@@ -1,3 +1,4 @@
+# %load q02_outlier_removal/build.py
 # Default imports
 import pandas as pd
 
@@ -7,5 +8,18 @@ ny_housing = pd.read_csv('data/train.csv')
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
 
 
-# Write your code here:
+def outlier_removal (dataset):
+    a = dataset.quantile(q=0.95,interpolation = 'lower')
+    Q_MasVnrArea = a['MasVnrArea']
+    Q_GrLivArea = a['GrLivArea']
+    Q_SalePrice = a['SalePrice']
+    #Q_LotShape = a['LotShape']
+    #Q_GarageType = a['GarageType']
+    df1 = housing_data.drop(housing_data[(housing_data['SalePrice']>Q_SalePrice)].index)
+    df2 = df1.drop(df1[(df1['MasVnrArea']>Q_MasVnrArea)].index)
+    df3 = df2.drop(df2[ (df2['GrLivArea']>Q_GrLivArea)].index)
+    return df3
 
+#df = outlier_removal(housing_data)
+#without_out.dropna(axis=0, how='any').shape
+#df.shape
