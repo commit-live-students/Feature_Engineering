3c3342bc194143158c10e9fe89a8dbe05d869a71
Mohammed-Sunasra <sunasramohd@gmail.com>
2017-11-04 17:11:05 +0000

    Done

--- q04_encoding/build.py
+++ q04_encoding/build.py
@@ -5,6 +5,14 @@ from sklearn.preprocessing import LabelEncoder
 ny_housing = pd.read_csv('data/train.csv')
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
 
+def encoding(df):
+    #df = df[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
+    le = LabelEncoder()
 
-# Write your code here:
 
+    #df['GarageType'] = le.fit_transform(df['GarageType'])
+    df['LotShape'] = le.fit_transform(df['LotShape'])
+    df_garage = pd.get_dummies(df['GarageType'])
+    #df.drop(['GarageType'],axis=1,inplace=True)
+    df_new = pd.concat([df,df_garage],ignore_index=True,axis=1)
+    return df_new
