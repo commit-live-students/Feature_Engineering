2c8f4bfad03ed8d919bcfdd6b542f51985bc266f
swapnilrt <swapnil13488@gmail.com>
2017-11-05 12:14:42 +0000

    Done

--- q02_outlier_removal/build.py
+++ q02_outlier_removal/build.py
@@ -13,7 +13,7 @@ def outlier_removal(dataset):
     p = {}
     for col in dataset:
         if dataset[col].dtype == np.int or dataset[col].dtype == np.float:
-            p[col] = dataset[col].quantile(.9545)
+            p[col] = dataset[col].quantile(.953)
 
     for col, p3 in p.items():
         dataset = dataset[dataset[col] < p3]
