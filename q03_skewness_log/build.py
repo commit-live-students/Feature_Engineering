2915980ad736a39fa489924ac3019880cbdf7925
pritambhaladhare <pritam.bhaladhare@gmail.com>
2017-11-05 20:15:51 +0000

    Done

--- q03_skewness_log/build.py
+++ q03_skewness_log/build.py
@@ -6,4 +6,15 @@ data = pd.read_csv('data/train.csv')
 
 
 # Write code here:
+def rf_rfe(df):
+    model = RandomForestClassifier()
+    X, y = data.iloc[:,:-1], data.iloc[:,-1]
+    nf= X.shape[1]/2
+    rfe = RFE(model, n_features_to_select=nf)
+    rfe = rfe.fit(X, y)
+    lst = [];
+    for i,rank in enumerate(rfe.ranking_):
+        if(rank==1):
+            lst.append(X.columns[i])
 
+    return lst
