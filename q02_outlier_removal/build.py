ba38d1b29cf035a7f33e14386684f25335b99095
ashwinij9 <ashwinijoshi9793@gmail.com>
2017-11-12 14:35:39 +0000

    Done

--- q02_outlier_removal/build.py
+++ q02_outlier_removal/build.py
@@ -10,6 +10,10 @@ def outlier_removal(housing_data):
     a=housing_data.MasVnrArea.quantile(0.95)
     b=housing_data.GrLivArea.quantile(0.95)
     c=housing_data.SalePrice.quantile(0.95)
+    print(a,b,c)
+
+    df1 = housing_data[(housing_data.MasVnrArea <= 499) & (housing_data.GrLivArea <= 2466) & (housing_data.SalePrice <=326100)]
+
 
     df = housing_data[(housing_data.MasVnrArea <= a) & (housing_data.GrLivArea <= b) & (housing_data.SalePrice <=c)]
-    return df
+    return df1
