367c0dff7826fe52f0df8f0d61e826b3ffb21580
apoorvabanubakode <apoorva.banubakode@gmail.com>
2017-11-12 17:09:39 +0000

    Done

--- q02_outlier_removal/build.py
+++ q02_outlier_removal/build.py
@@ -8,4 +8,14 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 
 # Write your code here:
-
+def outlier_removal(dataset):
+    cols = dataset._get_numeric_data().columns.tolist()
+    #print cols
+    q1 = dataset['MasVnrArea'].quantile(0.95)
+    q2 = dataset['GrLivArea'].quantile(0.95)
+    q3 = dataset['SalePrice'].quantile(0.95)
+    dataset = dataset.drop(dataset[(dataset['MasVnrArea']>q1)].index)
+    dataset = dataset.drop(dataset[(dataset['GrLivArea']>q2)].index)
+    dataset = dataset.drop(dataset[(dataset['SalePrice']>q3)].index)
+    #print dataset.shape
+    return dataset
