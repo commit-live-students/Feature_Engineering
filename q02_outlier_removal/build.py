3d5b393043b484d42a168cbe06bd66e1f274b451
arushikumar <arushikumar14@gmail.com>
2017-11-12 16:47:03 +0000

    Done

--- q02_outlier_removal/build.py
+++ q02_outlier_removal/build.py
@@ -1,6 +1,6 @@
 # Default imports
 import pandas as pd
-
+import numpy as np
 # Data
 ny_housing = pd.read_csv('data/train.csv')
 # Selecting 4 most relevant variables from the dataset fot the Cleaning and Preprocessing.
@@ -8,4 +8,14 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 
 # Write your code here:
-
+def outlier_removal(dataset):
+    cols = dataset._get_numeric_data().columns.tolist()
+    #print cols
+    q1 = dataset['MasVnrArea'].quantile(0.95)
+    q2 = dataset['GrLivArea'].quantile(0.95)
+    q3 = dataset['SalePrice'].quantile(0.95)
+    dataset = dataset.drop(dataset[(dataset['MasVnrArea']>q1)].index)
+    dataset = dataset.drop(dataset[(dataset['GrLivArea']>q2)].index)
+    dataset = dataset.drop(dataset[(dataset['SalePrice']>q3)].index)
+    #print dataset.shape
+    return dataset
