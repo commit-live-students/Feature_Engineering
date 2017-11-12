3fb15e739ed4bf5326b303e3f764c6523de6a03b
ashwinij9 <ashwinijoshi9793@gmail.com>
2017-11-12 14:28:17 +0000

    Done

--- q01_missing_value/build.py
+++ q01_missing_value/build.py
@@ -1,11 +1,22 @@
 # Default imports
 import pandas as pd
+from sklearn.preprocessing import Imputer
 
 # Data loading
 ny_housing = pd.read_csv('data/train.csv')
 # Selecting 4 most relevant variables along with target variable from the dataset fot the Cleaning and Preprocessing.
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
 
+def imputation(housing_data):
+    #total = housing_data.isnull().sum(axis=0).sort_values(ascending=False)
 
-# Write your code here:
+    imp_mean = Imputer(missing_values = 'NaN', strategy='mean')
+    imp_mean.fit(housing_data[['MasVnrArea']])
+    housing_data['MasVnrArea'] = imp_mean.transform(housing_data[['MasVnrArea']])
 
+    housing_data['GarageType'] = housing_data['GarageType'].fillna(housing_data['GarageType'].mode()[0])
+
+    df1 = pd.DataFrame(housing_data["MasVnrArea"])
+    df2 = pd.DataFrame(housing_data["GarageType"])
+
+    return(df1,df2) 
--- q02_outlier_removal/build.py
+++ q02_outlier_removal/build.py
@@ -7,12 +7,9 @@ ny_housing = pd.read_csv('data/train.csv')
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
 
 def outlier_removal(housing_data):
+    a=housing_data.MasVnrArea.quantile(0.95)
+    b=housing_data.GrLivArea.quantile(0.95)
+    c=housing_data.SalePrice.quantile(0.95)
 
-    a=housing_data.MasVnrArea.quantile(q=0.95)
-
-    b=housing_data.GrLivArea.quantile(q=0.95)
-    c=housing_data.SalePrice.quantile(q=0.95)
-    df1 = housing_data[(housing_data.MasVnrArea <= a) &(housing_data.GrLivArea <= b) &(housing_data.SalePrice <=c)]
-
-    return df1
-    
+    df = housing_data[(housing_data.MasVnrArea <= a) & (housing_data.GrLivArea <= b) & (housing_data.SalePrice <=c)]
+    return df
