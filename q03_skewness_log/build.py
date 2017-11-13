8e4b1d21adc318dafb6fd2f97cab98bdd21a03c5
pritambhaladhare <pritam.bhaladhare@gmail.com>
2017-11-13 16:58:44 +0000

    Done

--- q03_skewness_log/build.py
+++ q03_skewness_log/build.py
@@ -1,20 +1,15 @@
+# Default imports
 from scipy.stats import skew
 import pandas as pd
 import numpy as np
 
 data = pd.read_csv('data/train.csv')
 
+def skewness_log(df):
+    data['SalePrice'] = np.log(data['SalePrice'])
+    data['GrLivArea'] = np.log(data['GrLivArea'])
 
-# Write code here:
-def rf_rfe(df):
-    model = RandomForestClassifier()
-    X, y = data.iloc[:,:-1], data.iloc[:,-1]
-    nf= X.shape[1]/2
-    rfe = RFE(model, n_features_to_select=nf)
-    rfe = rfe.fit(X, y)
-    lst = [];
-    for i,rank in enumerate(rfe.ranking_):
-        if(rank==1):
-            lst.append(X.columns[i])
+    skew_1 = skew(data['GrLivArea'])
+    skew_2 = skew(data['SalePrice'])
 
-    return lst
+    return skew_1, skew_2
