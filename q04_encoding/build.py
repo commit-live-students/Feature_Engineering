57915b57e07609bb2a4948da5d452d89df35b133
NeelkanthMehta <neilstrong2003@yahoo.com>
2018-01-05 13:31:12 +0000

    Done

--- q04_encoding/build.py
+++ q04_encoding/build.py
@@ -7,4 +7,8 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 
 # Write your code here:
-
+def encoding(dataset):
+    df = dataset[['LotShape', 'GarageType']]
+    le = LabelEncoder()
+    df['LotShape'] = le.fit_transform(df['LotShape'])
+    return pd.concat([dataset, pd.get_dummies(dataset['GarageType'])],axis=1)
