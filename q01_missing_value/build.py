d0230d4cf973b848c9266b71e884e93129b7161b
nazirwa <nazirmail@gmail.com>
2018-01-07 03:01:38 +0000

    Done

--- q01_missing_value/build.py
+++ q01_missing_value/build.py
@@ -9,3 +9,26 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 # Write your code here:
 
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+from sklearn.preprocessing import Imputer
+def imputation(dataset):
+    imp_cont = Imputer(missing_values='NaN', strategy='mean')
+    imp_cont.fit(housing_data[['MasVnrArea']]) # fit for field
+    housing_data['MasVnrArea'] = imp_cont.transform(housing_data[['MasVnrArea']])
+    masdf = pd.DataFrame(housing_data['MasVnrArea'])
+
+    # Fill second field with None
+    housing_data['GarageType'] = housing_data['GarageType'].fillna('None')
+    grgdf = pd.DataFrame(housing_data['GarageType'])
+
+    return masdf, grgdf
+
+# print "#### Testing ####"
+# a, b = imputation(housing_data)
+# print type(a)
+# print type(b)
+# # print a
+# # print a.dtypes
+# print a.isnull().values.any()
+# print a.isnull().values.any()
