66dfdf069bb3fef2a6acc1162f36f7894e6207ac
shaikferozgithub <shaikferozmail@gmail.com>
2018-01-12 17:24:26 +0000

    Done

--- q02_outlier_removal/build.py
+++ q02_outlier_removal/build.py
@@ -1,3 +1,4 @@
+# %load q02_outlier_removal/build.py
 # Default imports
 import pandas as pd
 
@@ -6,6 +7,16 @@ ny_housing = pd.read_csv('data/train.csv')
 # Selecting 4 most relevant variables from the dataset fot the Cleaning and Preprocessing.
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
 
-
 # Write your code here:
+def outlier_removal(df):
+    vQuantile = df.quantile(q=0.95)
+    df = df.drop(df[(df['MasVnrArea']>vQuantile['MasVnrArea'])
+                    | (df['GrLivArea']>vQuantile['GrLivArea'])
+                    | (df['SalePrice']>vQuantile['SalePrice'])
+                    ].index)
+    return df
 
+a = outlier_removal(housing_data)
+print type(a)
+print a.shape
+print a.head()
--- q03_skewness_log/build.py
+++ q03_skewness_log/build.py
@@ -1,3 +1,4 @@
+# %load q03_skewness_log/build.py
 from scipy.stats import skew
 import pandas as pd
 import numpy as np
@@ -6,4 +7,15 @@ data = pd.read_csv('data/train.csv')
 
 
 # Write code here:
+def skewness_log(df):
+    skew_var = ['GrLivArea', 'SalePrice']
+    ret = []
+    for colName in skew_var:
+        df[colName] = np.log(df[colName])
+        ret.append(skew(df[colName]))
+    return ret[0], ret[1]
 
+print "#### Testint ####"
+a, b = skewness_log(data)
+print a, b
+print type(a), type(b)
