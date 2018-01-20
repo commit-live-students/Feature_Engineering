d237f89c4507b7bbe4fb81c187176f7b70f979ee
brockLesnar37 <ashlin+37@greyatom.com>
2018-01-20 07:30:32 +0000

    Done

--- q01_missing_value/build.py
+++ q01_missing_value/build.py
@@ -1,3 +1,4 @@
+# %load q01_missing_value/build.py
 # Default imports
 import pandas as pd
 
@@ -8,4 +9,21 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 
 # Write your code here:
+
+
+# Default imports
+import pandas as pd
+
+# Data
+ny_housing = pd.read_csv('data/train.csv')
+housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
+
+
+# Write your solution here :
+def imputation(dataset):
+    na = housing_data.isnull().sum()
+    print(na)
+    housing_data[['MasVnrArea']] = housing_data[['MasVnrArea']].fillna(housing_data.mean(), inplace=True)
+    housing_data[['GarageType']] = housing_data[['GarageType']].apply(lambda x: x.fillna(x.value_counts().index[0]))
+    return housing_data[['MasVnrArea']], housing_data[['GarageType']]
 
