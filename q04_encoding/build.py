5d20772fdb79e052c2d22783e5aa0c51d7b0913b
ms10031990 <ms10031990@yahoo.com>
2018-01-27 10:14:48 +0000

    Done

--- q04_encoding/build.py
+++ q04_encoding/build.py
@@ -5,6 +5,14 @@ from sklearn.preprocessing import LabelEncoder
 ny_housing = pd.read_csv('data/train.csv')
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
 
+def encoding(housing_data):
+    df=housing_data.copy()
+    #Label Encoding
+    lablel_encoder = LabelEncoder()
+    df['Lotshape'] = lablel_encoder.fit_transform(df['LotShape'])
 
-# Write your code here:
+    #Binary encoding
+    df1 = pd.get_dummies(df['GarageType'],drop_first=True)
 
+    result = pd.concat([df, df1], axis=1)
+    return result
