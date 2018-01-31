12ce34ae842a927b699f9bac13c5a5abfa2130fd
swanandbagve <swanandbagve@gmail.com>
2018-01-31 17:11:09 +0000

    Done

--- q01_missing_value/build.py
+++ q01_missing_value/build.py
@@ -8,4 +8,17 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 
 # Write your code here:
+def imputation(dataset):
+    housing_data=dataset.iloc[:,:-1]
+    df2=housing_data[list(housing_data._get_numeric_data().columns)]
+    df3=housing_data[list(set(housing_data.columns)-set(housing_data._get_numeric_data().columns))]
+    df=df2.copy()
+    df["MasVnrArea"] = df["MasVnrArea"].fillna(df["MasVnrArea"].mean())
+    df["GrLivArea"] = df["GrLivArea"].fillna(df["GrLivArea"].mean())
+    dfc=df3.copy() #mandatory as else  get error value is trying to be set on a copu
+    for column in dfc.columns:
+        dfc[column].fillna(dfc[column].mode()[0], inplace=True)
+    #dfc.isnull().values.any() validation shd return false
+    return df, dfc
 
+imputation(dataset=housing_data)
