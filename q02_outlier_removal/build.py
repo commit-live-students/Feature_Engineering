7204b848f2077b573da59bb3b7cc18159f918617
vp999 <patilvish@gmail.com>
2018-02-04 10:45:00 +0000

    Done

--- q02_outlier_removal/build.py
+++ q02_outlier_removal/build.py
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 # Default imports
 import pandas as pd
 
@@ -8,4 +15,21 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 
 # Write your code here:
+def outlier_removal (dataset):
+    target_column= 'SalePrice'
+    df= dataset.copy()
+    num_columns =df.select_dtypes(include=['float64','int64'])
 
+    num_columns = num_columns.drop(target_column,1)
+    #quantile_95= num_columns.quantile(0.9385)
+    quantile_95= num_columns.quantile(0.95)
+
+    for colname in num_columns:
+        quantile =  quantile_95[ colname ]
+        print colname, ":", quantile
+        df=df.drop(df[df[colname]>quantile].index)
+
+    return df
+
+df = outlier_removal(housing_data)
+print df.describe(include='all')
--- q03_skewness_log/build.py
+++ q03_skewness_log/build.py
@@ -6,4 +6,9 @@ data = pd.read_csv('data/train.csv')
 
 
 # Write code here:
+def skewness_log (data):
+    data['GrLivArea_1']=np.log(data['GrLivArea'])
+    data['SalePrice_1']=np.log(data['SalePrice'])
+    return (skew(data['GrLivArea_1']),skew(data['SalePrice_1']))
 
+print skewness_log(data)
