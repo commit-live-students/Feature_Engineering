ca78e3b90231a0ebf14039cdd0829a5c3db3f622
djocz <jocelynd789@gmail.com>
2018-02-05 16:48:40 +0000

    Done

--- q01_missing_value/build.py
+++ q01_missing_value/build.py
@@ -8,4 +8,12 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 
 # Write your code here:
+def imputation(dataset):
+    df2=dataset.copy()
+    df2["LotShape"] = df2["LotShape"].fillna('Reg')
+    df2["GarageType"] = df2["GarageType"].fillna('Attchd')
 
+    for col in ('MasVnrArea', 'GrLivArea', 'SalePrice'):
+        df2[col] = df2[col].fillna(df2[col].mean())
+
+    return df2.iloc[:,[0,1,4]],df2.iloc[:,[2,3]]
