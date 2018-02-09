53c701881768b45fed23d81e33c9a27135cbd86f
djocz <jocelynd789@gmail.com>
2018-02-09 17:55:14 +0000

    Done

--- q03_skewness_log/build.py
+++ q03_skewness_log/build.py
@@ -6,4 +6,11 @@ data = pd.read_csv('data/train.csv')
 
 
 # Write code here:
+def skewness_log(data):
+    data['SalePrice1'] = np.log(data['SalePrice'])
+    data['GrLivArea1'] = np.log(data['GrLivArea'])
+    skew_SalePrice = skew(data['SalePrice1'])
+    skew_GrLivArea = skew(data['GrLivArea1'])
+    return skew_GrLivArea,skew_SalePrice
 
+print skewness_log(data)
