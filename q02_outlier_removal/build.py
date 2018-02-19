494f3727929b9e703d35b442bb054003c04115ea
bhushan45 <bhushanshewale45@gmail.com>
2018-02-19 07:00:25 +0000

    Done

--- q02_outlier_removal/build.py
+++ q02_outlier_removal/build.py
@@ -8,4 +8,15 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 
 # Write your code here:
+def outlier_removal(dataset):
+    df=dataset.copy()
+    num_columns=df.select_dtypes(include=['float64','int64'])
 
+    quatile_95=num_columns.quantile(0.95)
+
+    for colname in num_columns:
+        quantile=quatile_95[colname]
+        print quantile
+        df=df.drop(df[df[colname]>quantile].index)
+
+    return df
