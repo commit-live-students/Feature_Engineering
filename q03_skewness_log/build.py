b492c27484da04b9e1a6d6daa9f8e15d5d23c34c
brijesh-upadhyay <brijesh_upadhyay@ymail.com>
2018-02-22 14:33:39 +0000

    Done

--- q03_skewness_log/build.py
+++ q03_skewness_log/build.py
@@ -1,9 +1,27 @@
-from scipy.stats import skew
+# %load q03_skewness_log/build.py
+import numpy as np 
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt  
+import seaborn as sns
+from scipy import stats
+#For some Statistics
+from scipy.stats import norm, skew
+from sklearn.preprocessing import Imputer
 
-data = pd.read_csv('data/train.csv')
+df = pd.read_csv('data/train.csv')
 
+def skewness_log(df):
+    
+    df['GrLivArea'] = np.log(df['GrLivArea'])
+    skewed_grLiv = skew(df['GrLivArea'])
+
+    df['SalePrice'] = np.log(df['SalePrice'])
+    skewed_grLiv1 = skew(df['SalePrice'])
+    return skewed_grLiv, skewed_grLiv1
+
+#skewness_log(df)
 
 # Write code here:
+
+
 
