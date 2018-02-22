a9fd7a33c4922d8e202d90e06f24ed3c3fbd59b8
brijesh-upadhyay <brijesh_upadhyay@ymail.com>
2018-02-22 15:52:23 +0000

    Done

--- q04_encoding/build.py
+++ q04_encoding/build.py
@@ -6,5 +6,11 @@ ny_housing = pd.read_csv('data/train.csv')
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
 
 
-# Write your code here:
+# Write your solution here :
+def encoding(housing_data):
+    housing_data['LotShape'] = housing_data['LotShape'].astype('category')
+    label_encoder = LabelEncoder()
+    housing_data['LotShape_label'] = label_encoder.fit_transform(housing_data['LotShape'])
+    housing_data = pd.get_dummies(housing_data, columns=['GarageType'])
+    return housing_data
 
