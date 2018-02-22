c032d0f0a913beb7fbfc3b91c646c5d14ec707f9
brijesh-upadhyay <brijesh_upadhyay@ymail.com>
2018-02-22 14:48:37 +0000

    Done

--- q03_skewness_sqrt/build.py
+++ q03_skewness_sqrt/build.py
@@ -1,10 +1,25 @@
-# Default imports
-from scipy.stats import skew
+# %load q03_skewness_log/build.py
+import numpy as np 
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt  
+import seaborn as sns
+from scipy import stats
+#For some Statistics
+from scipy.stats import norm, skew
+from sklearn.preprocessing import Imputer
 
-ny_housing = pd.read_csv('data/train.csv')
+df = pd.read_csv('data/train.csv')
 
+def skewness_sqrt(df):
+    
+    df['GrLivArea'] = np.sqrt(df['GrLivArea'])
+    skewed_grLiv = skew(df['GrLivArea'])
 
-# Write your Solution Here:
+    df['SalePrice'] = np.sqrt(df['SalePrice'])
+    skewed_grLiv1 = skew(df['SalePrice'])
+    return skewed_grLiv, skewed_grLiv1
+
+skewness_sqrt(df)
+
+#skewness_log(df)
 
