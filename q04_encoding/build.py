6b3ab181350ef7dcde922cdd13c85c2d09b33e7b
abkedar <anupbhande@gmail.com>
2018-02-28 14:47:20 +0000

    Done

--- q04_encoding/build.py
+++ q04_encoding/build.py
@@ -1,10 +1,41 @@
 # Default imports
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, LabelBinarizer, OneHotEncoder
 
 ny_housing = pd.read_csv('data/train.csv')
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
 
 
 # Write your code here:
+def encoding(housing_data):
+    data = housing_data.copy()
+    encoder = LabelEncoder()
+    binary = LabelBinarizer()
+    on_hot = OneHotEncoder()
+    data['LotShape'] = encoder.fit_transform(data['LotShape'])
+    datasets_dummies = pd.get_dummies(data['GarageType'])
+    d_df = pd.concat([data, datasets_dummies], axis=1)
+    return d_df.shape
 
+
+print encoding(housing_data)# Default imports
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, LabelBinarizer, OneHotEncoder
+
+ny_housing = pd.read_csv('data/train.csv')
+housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
+
+
+# Write your code here:
+def encoding(housing_data):
+    data = housing_data.copy()
+    encoder = LabelEncoder()
+    binary = LabelBinarizer()
+    on_hot = OneHotEncoder()
+    data['LotShape'] = encoder.fit_transform(data['LotShape'])
+    datasets_dummies = pd.get_dummies(data['GarageType'])
+    d_df = pd.concat([data, datasets_dummies], axis=1)
+    return d_df
+
+
+print encoding(housing_data)
