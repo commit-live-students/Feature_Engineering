687d6694920a5225f2d42c4b8531fb6959ef8044
sohelahmadkhan <sohelahmadkhan000@gmail.com>
2018-03-13 19:27:39 +0000

    Done

--- q01_missing_value/build.py
+++ q01_missing_value/build.py
@@ -9,3 +9,19 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 # Write your code here:
 
+def imputation(housing_data):
+
+    df2 = housing_data.copy()
+
+    lot_shape_imputer = df2['LotShape'].value_counts().index.max()
+    garage_type_imputer = df2['GarageType'].value_counts().index.max()
+
+    df2['LotShape'] = df2['LotShape'].fillna(lot_shape_imputer)
+    df2['GarageType'] = df2['GarageType'].fillna(garage_type_imputer)
+
+    for col in ('MasVnrArea', 'GrLivArea', 'SalePrice'):
+        df2[col] = df2[col].fillna(df2[col].mean())
+
+    return df2[['MasVnrArea', 'GrLivArea', 'SalePrice']], df2[['LotShape','GarageType']]
+
+#imputation(housing_data)
