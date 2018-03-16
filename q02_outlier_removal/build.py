89c3e90510794ca3f9c5d9e15e20b19cc9965d97
sohelahmadkhan <sohelahmadkhan000@gmail.com>
2018-03-16 16:55:47 +0000

    Done

--- q02_outlier_removal/build.py
+++ q02_outlier_removal/build.py
@@ -1,3 +1,4 @@
+# %load q02_outlier_removal/build.py
 # Default imports
 import pandas as pd
 
@@ -5,7 +6,24 @@ import pandas as pd
 ny_housing = pd.read_csv('data/train.csv')
 # Selecting 4 most relevant variables from the dataset fot the Cleaning and Preprocessing.
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
-
+housing_data
 
 # Write your code here:
+
+def outlier_removal(data):
+
+    df = data.copy()
+
+
+    q = df.quantile(0.95, axis = 0)
+
+    df = df.drop(df[df.MasVnrArea > q[0]].index)
+    df = df.drop(df[df.GrLivArea > q[1]].index)
+    df = df.drop(df[df.SalePrice > q[2]].index)
+
+    return df
+
+
+#outlier_removal(housing_data)
+
 
--- q04_encoding/build.py
+++ q04_encoding/build.py
@@ -1,3 +1,4 @@
+# %load q04_encoding/build.py
 # Default imports
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
@@ -7,4 +8,16 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 
 # Write your code here:
+def encoding(df):
+
+    le = LabelEncoder()
+    df['LotShape'] = le.fit_transform(df['LotShape'])
+    df['GarageType'] = le.fit_transform(df['GarageType'])
+
+    df1 = pd.get_dummies(df, columns = ['GarageType'])
+
+    return df1
+
+
+
 
