b50155dd670ca4c07a008ce45b5615ad1464c404
kvin15 <vinayakkhamkar@gmail.com>
2018-03-16 18:50:51 +0000

    Done

--- q04_encoding/build.py
+++ q04_encoding/build.py
@@ -7,4 +7,11 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 
 # Write your code here:
-
+def encoding(data):
+    data_copy = data.copy()
+    le = LabelEncoder()
+    data_copy.LotShape = le.fit_transform(data_copy.LotShape)
+    # We can actually drop the GarageType column as it is getting encoded,
+    # but test case requires it
+    df1 = pd.get_dummies(data_copy.GarageType, prefix="GarageType", prefix_sep="_")
+    return pd.concat([data_copy, df1],axis=1)
