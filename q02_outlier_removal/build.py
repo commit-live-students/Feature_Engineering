c24a3b23f5b0cb08f6089321305f6ed96f74e48f
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-03-17 16:06:46 +0000

    Done

--- q02_outlier_removal/build.py
+++ q02_outlier_removal/build.py
@@ -1,6 +1,6 @@
 # Default imports
 import pandas as pd
-
+import numpy as np
 # Data
 ny_housing = pd.read_csv('data/train.csv')
 # Selecting 4 most relevant variables from the dataset fot the Cleaning and Preprocessing.
@@ -8,4 +8,20 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 
 # Write your code here:
+def outlier_removal(dataset):
+    num_types = ['int8', 'int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    num_cols = dataset.select_dtypes(include=num_types).columns.tolist()
+    temp = dataset.loc[dataset.loc[:,'MasVnrArea'] != 0.0,'MasVnrArea']
+    dataset.loc[dataset.loc[:,'MasVnrArea'] == 0.0, 'MasVnrArea'] = temp.mean()
+    dataset.loc[dataset.loc[:,'MasVnrArea'].isnull(), 'MasVnrArea'] = temp.mean()
+    dataset.loc[dataset.loc[:,'GarageType'].isnull(), 'GarageType'] = 'Attchd'
+    for val in num_cols:
+        col_name = str(val) + '1'
+        temp_mean = dataset.loc[dataset.loc[:,val] != 0.0,val].mean()
+        temp_std = dataset.loc[dataset.loc[:,val] != 0.0,val].std()
+        dataset[col_name] = (dataset.loc[:,val] - temp_mean) / float(temp_std)
+        dataset.loc[dataset.loc[:,col_name] > 1.78, col_name] = np.nan
 
+    dataset.dropna(inplace=True)
+    dataset.drop(labels=dataset.iloc[:,[5,6,7]].columns.values, axis=1, inplace=True)
+    return dataset
