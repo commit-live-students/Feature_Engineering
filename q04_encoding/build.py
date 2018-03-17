0664298781dba721640367b05fd5beb90c25582a
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-03-17 17:22:01 +0000

    Done

--- q04_encoding/build.py
+++ q04_encoding/build.py
@@ -1,10 +1,19 @@
 # Default imports
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 
 ny_housing = pd.read_csv('data/train.csv')
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
 
 
 # Write your code here:
-
+def encoding(dataset):
+    le = LabelEncoder()
+    enc = OneHotEncoder(sparse=False)
+    dataset.loc[dataset.loc[:,'GarageType'].isnull(),'GarageType'] = 'Attchd'
+    dataset['GarageType'] = le.fit_transform(dataset['GarageType'])
+    temp = enc.fit_transform(dataset['GarageType'].reshape(-1,1))
+    dataset['LotShape'] = le.fit_transform(dataset['LotShape'])
+    ans = dataset.join(pd.DataFrame(temp), how='left')
+    return(ans)
