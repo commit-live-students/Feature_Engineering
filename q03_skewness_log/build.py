40200210e7e06983cc69d38665e67642c954d0ff
HardikaB <hardi.2607@gmail.com>
2018-06-22 15:29:50 +0000

    Done

--- q03_skewness_log/build.py
+++ q03_skewness_log/build.py
@@ -1,8 +1,20 @@
-from scipy.stats import skew
+# %load q03_skewness_log/build.py
+from scipy import stats
+from scipy.stats import skew,norm
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt  
 
 data = pd.read_csv('data/train.csv')
 
 
 # Write code here:
+def skewness_log(data):
+
+    data['GrLivAreaLT'] = np.log(data['GrLivArea'])
+    skewed_grLiv = skew(data['GrLivAreaLT'])
+    data['SalePriceLT'] = np.log(data['SalePrice'])
+    skewed_SaleP = skew(data['SalePriceLT'])
+    return skewed_grLiv, skewed_SaleP
+
