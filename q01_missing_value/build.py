94ae3dff5de1826cf6ef4b13411f4e7e01d91c6a
Nikita1993 <nikitajindal93@gmail.com>
2018-06-28 11:50:26 +0000

    Done

--- q01_missing_value/build.py
+++ q01_missing_value/build.py
@@ -8,3 +8,9 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 
 # Write your code here:
+def imputation(dataset):
+    na = housing_data.isnull().sum()
+    print(na)
+    housing_data[['MasVnrArea']] = housing_data[['MasVnrArea']].fillna(housing_data[['MasVnrArea']].mean())
+    housing_data[['GarageType']] = housing_data[['GarageType']].apply(lambda x: x.fillna(x.value_counts().index[0]))
+    return housing_data[['MasVnrArea']], housing_data[['GarageType']]
