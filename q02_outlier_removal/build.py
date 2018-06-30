bbee416dc14cb46549dc0d40cabfe230409ff81b
asdoc <asdocode@gmail.com>
2018-06-30 15:25:48 +0000

    Done

--- q02_outlier_removal/build.py
+++ q02_outlier_removal/build.py
@@ -1,5 +1,7 @@
+# %load q02_outlier_removal/build.py
 # Default imports
 import pandas as pd
+import numpy as np
 
 # Data
 ny_housing = pd.read_csv('data/train.csv')
@@ -7,4 +9,10 @@ ny_housing = pd.read_csv('data/train.csv')
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
 
 
-# Write your code here:
+def outlier_removal(dataset):
+    dataset = dataset[dataset['MasVnrArea'] < dataset['MasVnrArea'].quantile(0.947)]
+    dataset = dataset[dataset['GrLivArea'] < dataset['GrLivArea'].quantile(0.947)]
+    return dataset
+
+    
+
