4471097fc15a44e499424572469d3350ae4f02ba
Sumeshks29 <sumeshks29@gmail.com>
2018-09-14 06:59:44 +0000

    Done

--- q02_outlier_removal/build.py
+++ q02_outlier_removal/build.py
@@ -1,3 +1,4 @@
+# %load q02_outlier_removal/build.py
 # Default imports
 import pandas as pd
 
@@ -8,3 +9,14 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 
 # Write your code here:
+def outlier_removal(dataset):
+    data=housing_data.copy()
+    MasVnrArea=data['MasVnrArea'].quantile(.95)
+    GrLivArea=data['GrLivArea'].quantile(.95)
+    SalePrice=data['SalePrice'].quantile(.95)
+    data.drop(data[data['MasVnrArea']>MasVnrArea].index,inplace=True)
+    data.drop(data[data['GrLivArea']>GrLivArea].index,inplace=True)
+    data.drop(data[data['SalePrice']>SalePrice].index,inplace=True)
+    return data
+    
+
