1056e9fe1c6e99e2b8349a4a0a573711f369e782
rajeshbrid <rajesh_brid@hotmail.com>
2018-09-30 10:56:16 +0000

    Done

--- q01_missing_value/build.py
+++ q01_missing_value/build.py
@@ -1,10 +1,23 @@
+# %load q01_missing_value/build.py
 # Default imports
 import pandas as pd
+from sklearn.preprocessing import Imputer
 
 # Data loading
 ny_housing = pd.read_csv('data/train.csv')
 # Selecting 4 most relevant variables along with target variable from the dataset fot the Cleaning and Preprocessing.
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
 
-
 # Write your code here:
+
+def imputation(ny_housing):
+    imp_mean = Imputer(missing_values = 'NaN', strategy='mean')
+    imp_mean.fit(housing_data[['MasVnrArea']])
+    housing_data[['MasVnrArea']] = imp_mean.transform(housing_data[['MasVnrArea']])
+    housing_data['GarageType'] =  housing_data['GarageType'] .fillna( housing_data['GarageType'].mode()[0])
+    return pd.DataFrame(housing_data['MasVnrArea']),pd.DataFrame(housing_data['GarageType'])
+
+#housing_data.isnull().sum()
+
+
+
