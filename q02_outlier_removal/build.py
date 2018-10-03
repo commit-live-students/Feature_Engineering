9a284e0317931b047ee353d9fac63d9052c22d3b
rianboy18 <pranavr95.pn@gmail.com>
2018-10-03 04:59:49 +0000

    Done

--- q02_outlier_removal/build.py
+++ q02_outlier_removal/build.py
@@ -14,16 +14,10 @@ def outlier_removal(housing_data):
     housing_data['MasVnrArea']=housing_data['MasVnrArea'].fillna(mean)
     highly_occured=housing_data['GarageType'].loc[housing_data['GarageType'].notnull()].value_counts().index[0]
     housing_data['GarageType']=housing_data['GarageType'].fillna(highly_occured)
+    
+    return housing_data[(housing_data['MasVnrArea']<=housing_data['MasVnrArea'].quantile(0.95)) & (housing_data['GrLivArea']<=housing_data['GrLivArea'].quantile(0.95)) & (housing_data['SalePrice']<=housing_data['SalePrice'].quantile(0.95)) ]
 
-    
-#     housing_data['MasVnrArea']=housing_data.drop(housing_data['MasVnrArea']>456.0)
-#     housing_data['GrLivArea']=housing_data.drop(housing_data['GrLivArea']>2466.1)
-#     housing_data['SalePrice']=housing_data.drop(housing_data['SalePrice']>326100.0)
-#     return housing_data
-    housing_data = housing_data.drop(housing_data[(housing_data['GrLivArea']>3000) & (housing_data['GrLivArea']<6000)].index)
-    housing_data = housing_data.drop(housing_data[(housing_data['MasVnrArea']>3000) & (df['MasVnrArea']<6000)].index)
-    
-    return housing_data
+
 c=outlier_removal(housing_data)
 c
 
