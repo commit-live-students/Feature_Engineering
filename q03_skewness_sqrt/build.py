6682564ba8eda218bf36b2d2d5318b2e2754b34d
Abhimanyu22 <link2manyu@hotmail.com>
2018-11-03 09:14:50 +0000

    Done

--- q03_skewness_sqrt/build.py
+++ q03_skewness_sqrt/build.py
@@ -1,3 +1,4 @@
+# %load q03_skewness_sqrt/build.py
 # Default imports
 from scipy.stats import skew
 import pandas as pd
@@ -5,6 +6,17 @@ import numpy as np
 
 ny_housing = pd.read_csv('data/train.csv')
 
+def skewness_sqrt(ny_housing):
+    df = ny_housing.copy()
+    df['GrLivArea'] = np.sqrt(df['GrLivArea'])
+    df['SalePrice'] = np.sqrt(df['SalePrice'])
+    
+    skewed_grLiv = skew(df['GrLivArea'])
+    skewed_SalePrice = skew(df['SalePrice'])
+    
+    return skewed_grLiv, skewed_SalePrice
 
-# Write your Solution Here:
+skewness_sqrt(ny_housing)
+
+
 
