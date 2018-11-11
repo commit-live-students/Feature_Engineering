00b3f2eb0996b6d1d8491b6ea35bb4b4c5cd1ba6
prodigy803 <pushkaraj803@gmail.com>
2018-11-11 06:36:14 +0000

    Done

--- q03_skewness_log/build.py
+++ q03_skewness_log/build.py
@@ -1,3 +1,4 @@
+# %load q03_skewness_log/build.py
 from scipy.stats import skew
 import pandas as pd
 import numpy as np
@@ -6,3 +7,24 @@ data = pd.read_csv('data/train.csv')
 
 
 # Write code here:
+def skewness_log(data):
+    GrlivArea_array = np.array(data['GrLivArea'])
+    SalePrice_array = np.array(data['SalePrice'])
+    
+    GrLivArea95 = np.percentile(GrlivArea_array,0.95)
+    SalePrice95 = np.percentile(SalePrice_array,0.95)
+    
+    return skew(np.log(GrlivArea_array)),skew(np.log(SalePrice_array))
+
+
+
+
+
+
+
+
+
+
+
+
+
