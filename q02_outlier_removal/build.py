4053ecf5098bef6ff5b59322f53689a4f4bb3dcb
sunilhariharan <sunilhariharan@gmail.com>
2018-11-15 12:26:07 +0000

    Done

--- q02_outlier_removal/build.py
+++ q02_outlier_removal/build.py
@@ -1,3 +1,4 @@
+# %load q02_outlier_removal/build.py
 # Default imports
 import pandas as pd
 
@@ -8,3 +9,11 @@ housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 
 
 
 # Write your code here:
+def outlier_removal(dataset):
+    q1=housing_data.quantile(0.95)
+    hd =housing_data[housing_data['MasVnrArea']< 456.0]
+    hd1 =hd[hd['GrLivArea']< 2466.1]
+    hd2 =hd1[hd1['SalePrice']< 326100.0]
+    return hd2
+
+
--- q03_skewness_log/build.py
+++ q03_skewness_log/build.py
@@ -1,3 +1,4 @@
+# %load q03_skewness_log/build.py
 from scipy.stats import skew
 import pandas as pd
 import numpy as np
@@ -6,3 +7,11 @@ data = pd.read_csv('data/train.csv')
 
 
 # Write code here:
+def skewness_log(data):
+    s1=np.log(data['GrLivArea'])
+    skw1=skew(s1)
+    s2=np.log(data['SalePrice'])
+    skw2=skew(s2)
+    return skw1,skw2
+
+
