450adae9e355d5d463bc2e31aca583d42a59826a
akashhchatterjee <akashh.chatterjee@gmail.com>
2018-12-09 07:00:33 +0000

    Done

--- q01_missing_value/build.py
+++ q01_missing_value/build.py
@@ -1,10 +1,20 @@
+# %load q01_missing_value/build.py
 # Default imports
 import pandas as pd
-
+import numpy as np
 # Data loading
 ny_housing = pd.read_csv('data/train.csv')
 # Selecting 4 most relevant variables along with target variable from the dataset fot the Cleaning and Preprocessing.
 housing_data = ny_housing[['MasVnrArea', 'GrLivArea', 'LotShape', 'GarageType', 'SalePrice']]
 
-
 # Write your code here:
+def imputation(housing_data):
+    missing = housing_data.isnull().sum(axis=0).sort_values(ascending=False)
+    housing_data['MasVnrArea'].replace(0, np.nan, inplace= True)
+    housing_data['GarageType'] = housing_data['GarageType'].fillna('Attchd')
+    housing_data['MasVnrArea'] = housing_data['MasVnrArea'].fillna(housing_data['MasVnrArea'].mean())
+    return housing_data[['MasVnrArea']], housing_data[['GarageType']]
+
+imputation(housing_data)
+
+
